a0382c288c1fe9b75b073a295bf3eb3c81b36bdb
sarmadsaleem <sarmad.saleem@live.com>
2017-11-02 18:01:50 +0200

    add multiple viewports and save screenshots with relative dimensions

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const puppeteer = require('puppeteer');
 
-async function getPic() {
+(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
+  // Ann array of viewport sizes for different devices.
+  const viewports = [2560, 1920, 1680, 1440, 1366, 1280, 1024, 768, 480, 320];
+
   await page.goto('https://github.com/explore');
-  await page.setViewport({
-    width: 1920,
-    height: 1080
-  });
-  await page.screenshot({path: 'screenshot.png'});
 
-  await browser.close();
-}
+  for(let i=0; i < viewports.length; i++) {
+    let vw = viewports[i];
 
-getPic();
+    // The height doesn't matter since we are screenshotting the full page.
+    await page.setViewport({
+      width: vw,
+      height: 1000
+    });
+
+    await page.screenshot({
+      path: `screenshot-${vw}.png`,
+      fullPage: true
+    });
+  }
+
+  browser.close();
+})();
